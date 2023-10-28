b12aa7a5282b72885909a614fae652e3c16f5f50
Mansijain56 <mansijain5603@gmail.com>
2023-10-28 23:51:03 +0530

    deleted the files to rename

--- SaveModel/way2_joblib_way_result.py
+++
@@ -1,5 +0,0 @@
-import joblib
-# Load the trained model from the joblib file
-mp = joblib.load('house_joblib2')
-result = mp.predict([[3300]])
-print(result)
--- SaveModelWithSplitting/way1_pickle_way_testing.py
+++
@@ -1,8 +0,0 @@
-# to testing out pickle file
-import pickle
-
-with open('house_pickel','rb') as f:
-    mp = pickle.load(f)
-
-result = mp.predict([[3300]])
-print(result)
